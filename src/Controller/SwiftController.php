53be0a90c41c852fea2e2d98dc8c8f7ed83b0b14
tom <tom.guibard45@gmail.com>
2018-12-20 18:24:24 +0100

    Fait

--- src/Controller/SwiftController.php
+++ src/Controller/SwiftController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Satisfaction;
+use App\Form\SatisfactionType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,7 +14,7 @@ class SwiftController extends AbstractController
     /**
      * @Route("/swift", name="swift")
      */
-    public function index(\Swift_Mailer $swift_Mailer): Response
+    public function index(Request $request,\Swift_Mailer $swift_Mailer): Response
     {
         /*$message = (new \Swift_Message('Hello Email'))
             ->setFrom('cop.labo@gmail.com')
@@ -21,8 +24,22 @@ class SwiftController extends AbstractController
 
         // $swift_Mailer->send($message);
 
+
+        $startUp = new Satisfaction();
+        $form = $this->createForm(SatisfactionType::class, $startUp);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($startUp);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('start_up_index');
+        }
+
         return $this->render('emailQuestion/satisfaction.html.twig', [
             'controller_name' => 'SwiftController',
+            'form' => $form->createView()
         ]);
     }
 }
